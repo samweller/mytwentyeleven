8eeccca0c8a2446d23759d39382fada95f74de4c
Sam Weller <findme@samweller.com.au>
2011-12-05 19:02:03 +1100

    Remove some of the stuff I am using for my app

--- single.php
+++ single.php
@@ -22,44 +22,12 @@ get_header(); ?>
 
 					<?php get_template_part( 'content', 'single' ); ?>
 
-          <?php
-          
-            //echo get_post_custom_values('address');
-            echo get_post_meta( $post->ID, 'address', true );
-            echo get_post_meta( $post->ID, 'Suburb', true );
-            echo get_post_meta( $post->ID, 'www', true );
-          
-          ?>
-
-<div id="map_canvas" style="width:100%; height:500px"></div>
-
 					<?php comments_template( '', true ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
-
-
-    <script type="text/javascript">
-
-              jQuery().ready(function() {
-
-                    var latlng = new google.maps.LatLng(<?php echo get_post_meta( $post->ID, 'latlong', true ); ?>);
-                    var myOptions = {
-                      zoom: 12,
-                      center: latlng,
-                      mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
-                    var map = new google.maps.Map(document.getElementById("map_canvas"),
-                        myOptions);
-                    var marker = new google.maps.Marker({
-                         position: latlng, 
-                         map: map
-                     });
-
-          });
-        </script>
 
 
 <?php get_footer(); ?>
