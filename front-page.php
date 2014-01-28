b46df71044e3ac0e7b1519d40e9a875f6daf6abb
Sam Weller <sweller@realestate.com.au>
2014-01-28 20:22:17 +1100

    Add a custom template for the front page

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Front page template
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+  get_header(); ?>
+
+	<div id="primary">
+		<div id="content" role="main">
+	  
+		  <h1>Custom Home Page Lives here</h1>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php
+  get_footer();
+?>
